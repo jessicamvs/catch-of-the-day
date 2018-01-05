3f3cc88ac099f4dd9b23b14bf0a72a0592ee0e1b
Jessica Vasquez-Soltero <jmichellevs@gmail.com>
2018-01-04 17:22:38 -0800

    added state and created add inventory button and fish component

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,18 +1,38 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
- // use capital for all component names
 class StorePicker extends React.Component {
-  // every component needs the render method
+  // constructor() {
+  //   super();
+  //   this.goToStore = this.goToStore.bind(this)
+  // }
+
+  goToStore(event) {
+    event.preventDefault();
+    // first grab text from box
+    const storeId = this.storeInput.value;
+    // second transition from / to .store.:storeId
+    this.context.router.transitionTo(`/store/${storeId}`)
+  }
+
   render() {
     return (
-      <form className="store-selector">
-        { /* Hello this is a comment */}
+      <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
         <h2>Please Enter a Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input
+          type="text"
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+          ref={(input) => {this.storeInput = input}}
+          />
         <button type="submit"> Visit Store -></button>
       </form>
     )
   }
 }
 
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
+}
 export default StorePicker;
