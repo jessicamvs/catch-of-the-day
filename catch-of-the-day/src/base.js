7f25a2fb1398b2cd1a94ec4ca19c0a1998e54143
Jessica Vasquez-Soltero <jmichellevs@gmail.com>
2018-01-05 12:46:21 -0800

    connecting app to firebase db

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,10 @@
+import Rebase from 're-base';
+
+// connection to firebase db
+const base = Rebase.createClass({
+  apiKey: "AIzaSyB62Sj_BanybHGVAxuh0d3CzZKRNajr7gs",
+  authDomain: "catch-of-the-day-jessicamvs.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-jessicamvs.firebaseio.com"
+})
+
+export default base;
